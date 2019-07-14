4bdc539f8ea871037dacd2eec9fdc0cec180e6a4
NeumannE <52883750+NeumannE@users.noreply.github.com>
2019-07-14 15:19:21 +0200

    Update db_users_con.php

--- includes/header/db_users_con.php
+++ includes/header/db_users_con.php
@@ -2,7 +2,7 @@
 	$db_server_name = "localhost";
 	$db_user_name	= "root";
 	$db_password	= "";
-	$db_name 		= "account";
+	$db_name 	= "account";
 
 	$db_users_con 	= mysqli_connect($db_server_name, $db_user_name, $db_password, $db_name);
 ?>
